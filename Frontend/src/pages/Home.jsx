a6a852e462d6c6f47dc5fbd1c90446669cd6d47e
Rounak <rounakbasak12@gmail.com>
2025-01-01 01:10:47 +0530

    design login page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <div className=' bg-cover bg-center  bg-[url(https://images.unsplash.com/photo-1557404763-69708cd8b9ce?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8  flex justify-between flex-col w-full bg-red-400'>
+        <div className=' bg-cover bg-center  bg-[url(https://images.unsplash.com/photo-1557404763-69708cd8b9ce?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8  flex justify-between flex-col w-full bg-black'>
             <img className='w-20 ml-8 invert-0' src="https://freelogopng.com/images/all_img/1659768779uber-logo-white.png" alt="" />
             <div className='bg-white pb-7 py-4 px-4'>
                 <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function UserLogin() {
+  const [email, setEmal] = useState("")
+  const [password, setPassword] = useState("")
+  const [userData, setUserData] = useState({});
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    setUserData({ email, password })
+
+    setEmal("")
+    setPassword("")
+  }
+
   return (
-    <div>
-      User Login
+    <div className='p-7 flex flex-col justify-between h-screen'>
+      <div>
+        <img className='w-16 mb-10' src="https://logodix.com/logo/54423.png" alt="" />
+        <form action="" onSubmit={submitHandler}>
+          <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+
+          <input required className='bg-[#eeeeee] mb-7 rounded px-4  py-2 border w-full text-lg placeholder:text-base' value={email} onChange={(e) => setEmal(e.target.value)} type="email" placeholder='email@example.com' />
+          <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+          <input required className='bg-[#eeeeee] mb-7 rounded px-4  py-2 border w-full text-lg placeholder:text-base' value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='password' />
+
+          <button className='bg-[#111] text-[#fff] font-semibold mb-3 rounded px-4  py-2 w-full text-lg placeholder:text-base'>Login</button>
+          <p className='text-center'>New here? <Link to={"/signup"} className='text-blue-600 '>Create new Account</Link></p>
+
+        </form>
+      </div>
+      <div>
+        <Link to={"/captain-login"} className='bg-[#10b461] flex items-center justify-center text-[#fff] font-semibold mb-5 rounded px-4  py-2 w-full text-lg placeholder:text-base'>Sign in as Captain</Link>
+      </div>
+
     </div>
   )
 }
